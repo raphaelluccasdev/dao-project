76e8f908f9b95bc93195107c37980a8386e11333
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-09-23 10:00:01 +0000

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Departament obj = new Departament(1, "Books");
-        System.out.println(obj);;
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
