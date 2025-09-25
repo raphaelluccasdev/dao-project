f7aa10fdde9f0187467f5396e2f0755de60c52e5
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-09-25 10:09:43 +0000

    seller update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,13 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated! New id = " + seller.getId());
+
+
         System.out.println(seller);
 
     }
